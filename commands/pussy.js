a3a43689d1c178f82d8442e942aa2fe107f9bc84
Robert <nggroberto@gmail.com>
2018-09-21 20:22:33 +0200

    new commands fix

--- commands/pussy.js
+++ commands/pussy.js
@@ -1,7 +1,7 @@
 const { get } = require("snekfetch");
 
 exports.run = async function(client, message, args) {
-  if (!message.channel.nsfw) return message.response("🔞", "Cannot display NSFW content in a SFW channel.");
+  if (!message.channel.nsfw) return message.channel.send("🔞 Cannot display NSFW content in a SFW channel.");
   const msg = await message.channel.send(`<a:typing:492333599346130954> **${message.member.displayName}** is looking at pussies...`);
   const { body } = await get("https://nekobot.xyz/api/image?type=pussy");
   await msg.edit({
